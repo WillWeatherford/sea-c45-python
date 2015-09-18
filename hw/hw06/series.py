8af9043c1a4bd234ad145224fca6294262de32aa
Will Weatherford <weatherford.william@gmail.com>
2015-09-17 19:49:15 -0700

    removing hw06, updating bad_style.py and break_me.py

--- hw/hw06/series.py
+++
@@ -1,9 +0,0 @@
-def fibonacci(n):
-    '''
-    Returns the nth number from the Fibonacci sequence.
-
-    Example: fibonacci(0) == 1
-             fibonacci(1) == 1
-    '''
-    # probably recursive
-    return n
