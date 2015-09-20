0e29e84dd05295093579c7bd43c0deed606dd0a0
Will Weatherford <weatherford.william@gmail.com>
2015-09-20 16:19:09 -0700

    update changes to bad_style.py and break_me.pyc

---
+++ long_line.py
@@ -0,0 +1,7 @@
+print ('hhhhhhhhhhkhkfgggggggggggggggggggggggggggggggggggggggggggggggggggggg'
+       'fdkdkdkdkdkd'
+       'kjdjdjdj'
+       'gggggggggggggggggggg')
+
+print True or False or bool(str('jjjjjjjjjjjjjjjjjjjjjjjjjjjjjj')) \
+    and bool('ffffffffffffffffffffffffffff')
