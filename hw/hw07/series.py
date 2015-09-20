700456c372e763067dcbfa2242ea66a3cbc636fe
Will Weatherford <weatherford.william@gmail.com>
2015-09-20 16:53:40 -0700

    fibonacci function correct.

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -1,9 +1,17 @@
-def fibonacci(n):
+def fibonacci(n, m=0, l=1):
     '''
     Returns the nth number from the Fibonacci sequence.
 
-    Example: fibonacci(0) == 1
+    Example: fibonacci(0) == 0
              fibonacci(1) == 1
     '''
-    # probably recursive
-    return n
+    if n <= 0:
+        return m
+    else:
+        return fibonacci(n - 1, l, m + l)
+
+def lucas(n):
+    return 0
+
+def sum_series(n):
+    return 0
