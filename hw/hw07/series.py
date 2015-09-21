08e1ae0e1674da2ddf59b4ff06df71ca4530abbc
Will Weatherford <weatherford.william@gmail.com>
2015-09-20 18:17:41 -0700

    sum_series working correctly

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -1,14 +1,11 @@
-def fibonacci(n, seq1=0, seq2=1):
+def fibonacci(n):
     '''
     Returns the nth number from the Fibonacci sequence.
 
     Example: fibonacci(0) == 0
              fibonacci(1) == 1
     '''
-    if n <= 0:
-        return seq1
-    else:
-        return fibonacci(n - 1, seq2, seq1 + seq2)
+    return sum_series(n, 0, 1)
 
 
 def lucas(n):
@@ -17,9 +14,16 @@ def lucas(n):
 
     Example: lucas(0) == 2
              lucas(1) == 1
+             lucas(3) == 3
     '''
-    return fibonacci(n, 2, 1)
+    return sum_series(n, 2, 1)
 
 
-def sum_series(n, m, l):
-    return 0
+def sum_series(n, seq1=0, seq2=1):
+    '''
+    Returns the nth in a sequence starting with seq1 and seq2.
+    '''
+    if n <= 0:
+        return seq1
+    else:
+        return sum_series(n - 1, seq2, seq1 + seq2)
