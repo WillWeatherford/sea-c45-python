2c63aadff7fe3c2332455c2eb5548d21810c874d
Will Weatherford <weatherford.william@gmail.com>
2015-09-27 22:22:59 -0700

    T menu works mostly now

--- hw/hw11/mailroom_madness.py
+++ hw/hw11/mailroom_madness.py
@@ -15,10 +15,25 @@ Please enter a name, or choose from the following:
 
 list - Print a list of previous donors
 
-quit - Return to main menur's full name.
+quit - Return to main menu.
 '''
 AMOUNT_PROMPT = '''
 Please enter a donation amount or 'quit':
+'''
+LETTER_TEMPLATE = '''
+Dear {name},
+
+Thank you so much for your kind donation of ${amount}. We here at the Foundation
+for Homeless Whales greatly appreciate it. Your money will go towards creating
+new oceans on the moon for whales to live in.
+
+Thanks again,
+
+Jim Grant
+
+Director, F.H.W.
+
+Press enter to continue.
 '''
 
 
@@ -30,31 +45,30 @@ def repl(data, prompt, validator=None):
     valid, and None if not valid
     """
     while True:
-        user_input = input(prompt)
+        user_input = raw_input(prompt)
+        if user_input in ('q', 'Q', 'quit'):
+            print 'Quitting...'
+            return
         if validator:
             result = validator(data, user_input)
             if result:
-                if result in ('Q','quit'):
-                    print 'Quitting...'
-                    return
-                elif 'Invalid' in result:
+                if 'Invalid' in str(result):
                     print result
                 else:
                     print result
                     return result
+        else:
+            return
 
 
 def main_menu(data, user_input):
     '''
     main program loop;
     can redirect to sub-loops
-
     '''
-    if user_input == 'quit':
-        return user_input
-    elif user_input == 'T':
+    if user_input in ('t', 'T'):
         repl(data, T_MENU_PROMPT, t_menu)
-    elif user_input == 'R':
+    elif user_input in ('r', 'R'):
         repl(data, '', r_menu)
     else:
         return 'Invalid command.'
@@ -64,13 +78,14 @@ def t_menu(data, user_input):
     if user_input == 'list':
         #################
         return
-    elif user_input == 'quit':
-        return user_input
     else:
         name = user_input
         if is_valid_name(name):
             amount = repl(data, AMOUNT_PROMPT, is_valid_amount)
-            data = add_to_data(data, name, amount)
+            if amount:
+                data = add_to_data(data, name, amount)
+                letter = LETTER_TEMPLATE.format(name=name, amount=amount)
+                return repl(data, letter)
         else:
             return 'Invalid Name.'
 
@@ -87,7 +102,7 @@ def is_valid_name(name):
 
 def is_valid_amount(data, user_input):
     try:
-        return round(float(user_input), 2)
+        return str(round(float(user_input), 2))
     except ValueError:
         return 'Invalid Amount'
 
@@ -103,90 +118,89 @@ def add_to_data(data, name, amount):
     return data
 
 
-#######################
-# PSEUDO VERSION
-def main_menu():
-    '''
-    main program loop;
-    can redirect to sub-loops
-
-    '''
-
-    donor_data = {}
-    while True:
-        print 'Choose a comand:'
-        print "T: Send a thank you note."
-        print "R: Print report."
-        print "Q: Quit."
-        user_input = input('Your command:')
-
-        if user_input == 'Q':
-            print 'Quitting...'
-            return
-
-        elif user_input == 'T':
-            t_menu()
-        elif user_input == 'R':
-            report(donor_data)
-        else:
-            print 'Invalid command.'
-
-
-def t_menu():
-    '''
-    Send Thank You menu loop
-    '''
-    while True:
-        print list of options:
-        print "L: see a list of donor names"
-        print "Q: quit to main menu"
-        print "Or type a donor's full name"
-        user_input = input('Your Command:')
-
-        if user_input == 'Q':
-            break and return
-
-        elif user_input == 'L':
-            print_list and return
-        else:
-            if is_valid_name(user_input):
-                name = fetch_from_data(user_input)
-                if not name:
-                    add_to_data(user_input)
-
-                amount = input('Enter Amount:')
-
-                if is_valid_amount(amount):
-                    add_to_data(name, amount)
-                    write_formatted_email(name, amount)
-                        break and return
-                else:
-                    print 'invalid donation amount'
-            else:
-                print 'invalid donor name'
-
-
-def report(donor_data):
-    '''
-    print one super neatly formatted row per donor
-    '''
-    print 'Report Table Header'
-    sorted_list = donor_data.keys().sort()
-    for donor in sorted_list:
-        donations = donor_data[donor]
-        num = len(donations)
-        total = sum(donations)
-        avg = num / total
-
-        '{} super neat format {} {} {}'.format(donor, num, total, avg)
-
-    return
-
-
 if __name__ == '__main__':
     data = {}
-    try:
-        repl(data, MAIN_MENU_PROMPT, main_menu)
-    except Exception as e:
-        print 'ERROR IN MAIN LOOP'
-        raise e
+    repl(data, MAIN_MENU_PROMPT, main_menu)
+
+
+#######################
+# PSEUDO VERSION
+# def main_menu():
+#     '''
+#     main program loop;
+#     can redirect to sub-loops
+
+#     '''
+
+#     donor_data = {}
+#     while True:
+#         print 'Choose a comand:'
+#         print "T: Send a thank you note."
+#         print "R: Print report."
+#         print "Q: Quit."
+#         user_input = input('Your command:')
+
+#         if user_input == 'Q':
+#             print 'Quitting...'
+#             return
+
+#         elif user_input == 'T':
+#             t_menu()
+#         elif user_input == 'R':
+#             report(donor_data)
+#         else:
+#             print 'Invalid command.'
+
+
+# def t_menu():
+#     '''
+#     Send Thank You menu loop
+#     '''
+#     while True:
+#         print list of options:
+#         print "L: see a list of donor names"
+#         print "Q: quit to main menu"
+#         print "Or type a donor's full name"
+#         user_input = input('Your Command:')
+
+#         if user_input == 'Q':
+#             break and return
+
+#         elif user_input == 'L':
+#             print_list and return
+#         else:
+#             if is_valid_name(user_input):
+#                 name = fetch_from_data(user_input)
+#                 if not name:
+#                     add_to_data(user_input)
+
+#                 amount = input('Enter Amount:')
+
+#                 if is_valid_amount(amount):
+#                     add_to_data(name, amount)
+#                     write_formatted_email(name, amount)
+#                         break and return
+#                 else:
+#                     print 'invalid donation amount'
+#             else:
+#                 print 'invalid donor name'
+
+
+# def report(donor_data):
+#     '''
+#     print one super neatly formatted row per donor
+#     '''
+#     print 'Report Table Header'
+#     sorted_list = donor_data.keys().sort()
+#     for donor in sorted_list:
+#         donations = donor_data[donor]
+#         num = len(donations)
+#         total = sum(donations)
+#         avg = num / total
+
+#         '{} super neat format {} {} {}'.format(donor, num, total, avg)
+
+#     return
+
+
+
