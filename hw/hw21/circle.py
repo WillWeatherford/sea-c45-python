bf1da1741804c16a8fb401914943dca6c358927d
Will Weatherford <weatherford.william@gmail.com>
2015-10-05 22:38:58 -0700

    added properties for _diameter to Circle

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -14,3 +14,16 @@ class Circle(object):
         self.radius = radius
         self.diameter = radius * 2
         self.area = math.pi * radius ** 2
+
+    def _get_d(self):
+        return self._diameter
+
+    def _set_d(self, value):
+        self._diameter = value
+        self.radius = value / 2
+        self.area = math.pi * (value / 2) ** 2
+
+    def _del_d(self):
+        del self._diameter
+
+    diameter = property(_get_d, _set_d, _del_d, doc='docstring')
