844fbc39d528b384504777e8cdb3fcfd16c735ed
Will Weatherford <weatherford.william@gmail.com>
2015-10-06 21:53:53 -0700

    All tests passed

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -12,18 +12,56 @@ class Circle(object):
         Initialize a Circle object with a given radius parameter.
         '''
         self.radius = radius
-        self.diameter = radius * 2
-        self.area = math.pi * radius ** 2
+        self._diameter = float(radius) * 2
+        self._area = float(radius) ** 2 * math.pi
+
+    def __str__(self):
+        return 'Circle with radius: {:.6f}'.format(self.radius)
+
+    def __repr__(self):
+        return 'Circle({})'.format(self.radius)
+
+    def __add__(self, other):
+        return Circle(self.radius + other.radius)
+
+    def __mul__(self, n):
+        return Circle(self.radius * n)
+
+    def __eq__(self, other):
+        return self.radius == other.radius
+
+    def __ne__(self, other):
+        return self.radius != other.radius
+
+    def __lt__(self, other):
+        return self.radius < other.radius
+
+    def __gt__(self, other):
+        return self.radius > other.radius
+
+    def __le__(self, other):
+        return self.radius <= other.radius
+
+    def __ge__(self, other):
+        return self.radius >= other.radius
 
     def _get_d(self):
         return self._diameter
 
     def _set_d(self, value):
-        self._diameter = value
         self.radius = value / 2
-        self.area = math.pi * (value / 2) ** 2
+        self._diameter = value
+        self._area = math.pi * (value / 2) ** 2
 
     def _del_d(self):
         del self._diameter
 
-    diameter = property(_get_d, _set_d, _del_d, doc='docstring')
+    def _get_a(self):
+        return self._area
+
+    def _del_a(self):
+        del self._area
+
+    diameter = property(_get_d, _set_d, _del_d, doc='Diameter docstring')
+
+    area = property(_get_a, None, _del_a, doc='Area docstring')
