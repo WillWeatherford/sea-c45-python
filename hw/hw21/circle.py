1e1972bcc7ce351c1ba7431ba1de8d9826670719
Will Weatherford <weatherford.william@gmail.com>
2015-10-06 22:25:40 -0700

    Tried to use a fancy shortcut way of defining all the "magic methods" for operators e.g. __mul__(). They all passed the test except for __add__() and __eq__(). Couldn t really figure out why.

--- hw/hw21/circle.py
+++ hw/hw21/circle.py
@@ -15,17 +15,28 @@ class Circle(object):
         self._diameter = float(radius) * 2
         self._area = float(radius) ** 2 * math.pi
 
+        # for f in ['__add__', '__eq__', '__ge__', '__gt__',
+        #           '__le__', '__lt__', '__ne__']:
+        #     setattr(self, f,
+        #             lambda self, other: getattr(self.radius, f)(other.radius))
+
     def __str__(self):
         return 'Circle with radius: {:.6f}'.format(self.radius)
 
     def __repr__(self):
         return 'Circle({})'.format(self.radius)
 
+    def __mul__(self, n):
+        return Circle(self.radius * n)
+
     def __add__(self, other):
         return Circle(self.radius + other.radius)
 
-    def __mul__(self, n):
-        return Circle(self.radius * n)
+    def __radd__(self, other):
+        if other == 0:
+            return self.radius
+        else:
+            return self.__add__(other.radius)
 
     def __eq__(self, other):
         return self.radius == other.radius
