cc64dfbe28df330017cf7dbf074387a6b86908e2
Will Weatherford <weatherford.william@gmail.com>
2015-10-07 08:20:58 -0700

    updated trigrams.py in hw13 branch to the version in hw21

--- hw/hw13/trigrams.py
+++ hw/hw13/trigrams.py
@@ -1,44 +1,29 @@
 import sys
+import time
 import random
 
-trigrams = {}
-
-def repl(prompt, validator=None):
-    while True:
-        user_input = raw_input(prompt)
-        if user_input in ('q', 'Q', 'quit', 'Quit'):
-            print 'Quitting'
-            return
-        if validator:
-            result = validator(user_input)
-            if result:
-                if 'Invalid' in str(result):
-                    print result
-                else:
-                    return result
-        else:
-            return
+# Hannah$ cd Documents/CodeFellows/sea-c45-python/hw/hw13
 
 
-def main_menu(user_input):
-    try:
-        textfile = open(filename, 'r')
-    except:
-        return 'Invalid Filename'
+def main(filename, new_story_length=100):
 
-    lines = textfile.readlines()
-    textfile.close()
+    words = words_from_file(filename)
+
+    trigrams = trigrams_from_words(words)
+
+    output = story_from_trigrams(trigrams, new_story_length)
+
+    print '-------------------------------------'
+    return output
 
 
-def main(filename):
-    textfile = open(filename, 'r')
-    lines = textfile.readlines()
-    textfile.close()
+def words_from_file(filename):
+    with open(filename, 'r') as textfile:
+        return [word.strip('\n') for l in textfile.readlines()
+                for word in l.split(' ')]
 
-    words = []
-    for l in lines:
-        words.extend(l.split(' '))
 
+def trigrams_from_words(words):
     trigrams = {}
     for i, w in enumerate(words):
         if i + 2 < len(words):
@@ -50,29 +35,36 @@ def main(filename):
             if onetwo not in trigrams.keys():
                 trigrams[onetwo] = []
             trigrams[onetwo].append(three)
-
-    new_words = create_new_words(trigrams, 100)
-    print ' '.join(new_words)
+    return trigrams
 
 
-def create_new_words(trigrams, num, new_words=[]):
-    if len(new_words) >= num:
-        return new_words
+def story_from_trigrams(trigrams, num, new_words=[]):
+    if len(new_words) >= int(num) or num < 3:
+        return ' '.join(new_words)
+
     if not new_words:
         starting_two = random.choice(trigrams.keys())
         new_words.extend(starting_two.split(' '))
+
     if len(new_words) > 1:
         onetwo = '{} {}'.format(new_words[-2], new_words[-1])
         if onetwo not in trigrams.keys():
             onetwo = random.choice(trigrams.keys())
         three = random.choice(trigrams[onetwo])
         new_words.append(three)
-        return create_new_words(trigrams, num, new_words)
+        return story_from_trigrams(trigrams, num, new_words)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 1:
-        print 'No filename given.'
-    else:
+    try:
         filename = sys.argv[1]
-        main(filename)
+        try:
+            new_story_length = sys.argv[2]
+        except IndexError:
+            print('No new story length given; defaulting to new story length'
+                  ' of 100 words')
+            new_story_length = 100
+        output = main(filename, new_story_length)
+        print output
+    except IndexError:
+        print 'No filename given.'
