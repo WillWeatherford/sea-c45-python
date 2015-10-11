be9bfca7b3cbaf4f2344f0bb164f253c6705d0ce
Will Weatherford <weatherford.william@gmail.com>
2015-10-11 11:34:31 -0700

    IterateMe2 class now matches xrange from Python 2.7. Adjusted the __iter__ method to re-set the self.current attr to the start value.

--- tasks/task23/iterator_1.py
+++ tasks/task23/iterator_1.py
@@ -34,15 +34,18 @@ class IterateMe2(object):
     returns the sequence of numbers from zero to 4
     ( like xrange(4) )
     """
-    def __init__(self, stop=5):
-        self.current = -1
+    def __init__(self, start, stop, step=1):
+        self.current = start
+        self.start = start
         self.stop = stop
+        self.step = step
 
     def __iter__(self):
+        self.current = int(self.start)
         return self
 
     def __next__(self):
-        self.current += 1
+        self.current += self.step
         if self.current < self.stop:
             return self.current
         else:
@@ -53,4 +56,13 @@ if __name__ == "__main__":
 
     print("first version")
     for i in IterateMe1():
+        print(i)
+    print("second version")
+    it = IterateMe2(2, 20, 2)
+    for i in it:
+        if i > 10:
+            break
+        print(i)
+    print('picking iterator back up')
+    for i in it:
         print(i)
